8331ff2126c58eaac9c45e660872fc82b1a4d6b9
Mourougen MRMT <97475510+MourouMRMT@users.noreply.github.com>
2023-12-22 09:49:06 +0100

    revision post revue

--- Monitoring/ESP32_mon_server.ino
+++ Monitoring/ESP32_mon_server.ino
@@ -4,13 +4,14 @@
 #include <ESPAsyncWebSrv.h>
 #include "SPIFFS.h"
 #include <Arduino_JSON.h>
+//#include <FS.h>
 
 #define ONBOARD_LED  2
 
-//ESP32 IP @: 172.20.10.9
-// Replace with your network credentials
-const char* ssid = "Mourou";
-const char* password = "bimbapbam";
+//ESP32 IP @: 192.168.42.145
+
+const char* ssid = "Rpi-Hotspot";
+const char* password = "raspint0";
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
@@ -29,13 +30,15 @@ unsigned short timerDelay = 3000;
 unsigned short r_T;
 unsigned short r_TdC;
 unsigned short r_ef;
+
 // Get Can frame and return JSON object
 String getSensorReadings(){
-  readings["temperature"] = String(r_T);
+  readings["temperaturebat"] = String(r_T);
   readings["TauxdeCharge"] =  String(r_TdC);
-  readings["etatfeux"] = String(r_ef);
+  readings["etatfeuxstop"] = String(r_ef);
   readings["vitmoteur"]=String(r_TdC);
   String jsonString = JSON.stringify(readings);
+
   return jsonString;
 }
 
@@ -46,6 +49,18 @@ void initSPIFFS() {
   }
   Serial.println("SPIFFS mounted successfully");
 }
+
+// Fonction pour enregistrer les données dans un fichier
+/*void saveDataToFile(String data) {
+  File file = SPIFFS.open("/data.txt", "a");
+  if (!file) {
+    Serial.println("Erreur lors de l'ouverture du fichier");
+    return;
+  }
+
+  file.println(data);
+  file.close();
+}*/
 
 // Initialize WiFi
 void initWiFi() {
@@ -65,19 +80,25 @@ void notifyClients(String sensorReadings) {
 
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
   AwsFrameInfo *info = (AwsFrameInfo*)arg;
-  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) {
+  if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT) 
+  {
     //data[len] = 0;
     //String message = (char*)data;
     // Check if the message is "getReadings"
-    //if (strcmp((char*)data, "getReadings") == 0) {
-      //if it is, send current sensor readings
-      String sensorReadings = getSensorReadings();
-      Serial.print(sensorReadings);
-      notifyClients(sensorReadings);
+    //if (strcmp((char*)data, "getReadings") == 0) 
+	//{
+	//if it is, send current sensor readings
+	String sensorReadings = getSensorReadings();
+	// Enregistrer les données dans un fichier
+	saveDataToFile(sensorReadings);
+	Serial.print(sensorReadings);
+	notifyClients(sensorReadings);
+	ws.text(info->clientId, "acknowledged");
     //}
   }
 }
 
+/*
 void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
   switch (type) {
     case WS_EVT_CONNECT:
@@ -94,9 +115,9 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
       break;
   }
 }
-
+*/
 void initWebSocket() {
-  ws.onEvent(onEvent);
+  //ws.onEvent(onEvent);
   server.addHandler(&ws);
 }
 
@@ -114,13 +135,25 @@ void setup() {
   initSPIFFS();
   initWebSocket();
 
-  // Web Server Root URL
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(SPIFFS, "/index.html", "text/html");
-  });
-
+	// Web Server Root URL
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+	request->send(SPIFFS, "/index.html", "text/html");
+	});
+/*
+	// Serve the data file when requested
+	server.on("/download", HTTP_GET, [](AsyncWebServerRequest *request){
+	// Open the file for reading
+	File file = SPIFFS.open("/data.txt", "r");
+	if (!file) {
+		request->send(404, "text/plain", "Fichier non trouvé");
+		return;
+	}
+*/
+	// Serve the file as plain text
+	request->send(SPIFFS, "/data.txt", "text/plain");
+	file.close();
+	
   server.serveStatic("/", SPIFFS, "/");
-
   // Start server
   server.begin();
 
